49a3346c5d18507548dfa23d065802d0c444a2b8
Bakhtiyor <Teaddyson@gmail.com>
2024-12-18 21:50:06 +0300

    cors fixed

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -36,9 +37,6 @@ func main() {
 		pkg.Module,
 		fx.Invoke(routes.RegisterRoutes),
 		fx.Invoke(StartServer),
-		fx.Invoke(func() {
-			go processClicks()
-		}),
 	)
 
 	if err := app.Start(context.Background()); err != nil {
@@ -61,27 +59,27 @@ func main() {
 	log.Println("Application stopped")
 }
 
-func NewGinEngine() *gin.Engine {
-	return gin.Default()
+func NewGinEngine(cfg *config.Config) *gin.Engine {
+	engine := gin.Default()
+
+	config := cors.Config{
+		AllowOrigins:     strings.Split(cfg.AppFrontUrl, ","),
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*"},  // Разрешённые заголовки
+		ExposeHeaders:    []string{"*"},  // Заголовки, доступные клиенту
+		AllowCredentials: true,           // Для запросов с куками
+		MaxAge:           12 * time.Hour, // Время кеширования CORS-политики
+	}
+
+	engine.Use(cors.New(config))
+
+	return engine
 }
 
 func StartServer(router *gin.Engine, cfg *config.Config) {
 	go func() {
-		config := cors.Config{
-			AllowOrigins:     []string{cfg.AppFrontUrl, "http://localhost:5173"},  // Разрешённый домен
-			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-			AllowHeaders:     []string{"*"},  // Разрешённые заголовки
-			ExposeHeaders:    []string{"*"},  // Заголовки, доступные клиенту
-			AllowCredentials: true,           // Для запросов с куками
-			MaxAge:           12 * time.Hour, // Время кеширования CORS-политики
-		}
-
-		router.Use(cors.New(config))
-
-		log.Printf("Starting server on port %s", cfg.AppPort)
-
 		if cfg.IsProduction() {
-			certmagic.DefaultACME.Agreed = true                          
+			certmagic.DefaultACME.Agreed = true
 			certmagic.DefaultACME.Email = cfg.AppEmail
 			certmagic.DefaultACME.CA = certmagic.LetsEncryptProductionCA
 			certmagic.Default.Storage = &certmagic.FileStorage{Path: "./certmagic-storage/certs"}
@@ -92,15 +90,10 @@ func StartServer(router *gin.Engine, cfg *config.Config) {
 			if err != nil {
 				log.Fatalf("Failed to start HTTPS server: %v", err)
 			}
-		}else {
+		} else {
 			if err := router.Run("0.0.0.0:" + cfg.AppPort); err != nil {
 				log.Fatalf("Failed to start server: %v", err)
 			}
 		}
 	}()
-}
-
-func processClicks() {
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
 }
--- internal/middleware/telegram_hash_check_middleware.go
+++ internal/middleware/telegram_hash_check_middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"strings"
 	"example.com/v2/config"
 	"example.com/v2/pkg/telegram"
 	"github.com/gin-gonic/gin"
@@ -14,12 +15,24 @@ func TelegramHashCheck(cfg *config.Config) gin.HandlerFunc {
 			return
 		}
 
-		if !telegram.ValidateInitData(c.GetHeader("X-Telegram-Init-Data"), cfg.Telegram.Token) {
+		if !isValid(cfg.Telegram.Token, c.GetHeader("X-Telegram-Init-Data")) {
 			c.JSON(401, gin.H{"error": "Invalid initData"})
 			c.Abort()
 			return
 		}
-		
+
 		c.Next()
 	}
+}
+
+func isValid(botTokens, initData string) bool {
+	tokens := strings.Split(botTokens, ",")
+
+	for _, token := range tokens {
+		if telegram.ValidateInitData(initData, token) {
+			return true
+		}
+	}
+
+	return false
 }
